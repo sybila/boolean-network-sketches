45eee8ed9c010a6c15babba5775c31fd684de17d
Ondřej Huvar <492849@mail.muni.cz>
2022-09-24 15:01:12 +0200

    improve cli argument parsing

--- src/bin/infer_networks_attractors.rs
+++ src/bin/infer_networks_attractors.rs
@@ -1,49 +1,57 @@
+use bn_inference_tool::attractor_inference::*;
+
+use clap::Parser;
+
 #[allow(unused_imports)]
 use hctl_model_checker::analysis::{analyse_formula, model_check_formula_unsafe, PrintOptions};
 
-use bn_inference_tool::attractor_inference::*;
-
-use std::env;
 use std::fs::{read_to_string, File};
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 use std::time::SystemTime;
 
+/// Structure to collect CLI arguments
+#[derive(Parser)]
+#[clap(author="Ondrej Huvar", version, about="Inference of BNs from partially defined model and attractors.")]
+struct Arguments {
+    /// Path to the file with BN model in aeon format
+    model_path: String,
+
+    /// Path to the file with attractor data
+    attractor_data_path: String,
+
+    /// Forbid all attractors not containing specified states
+    #[clap(short, long, takes_value = false)]
+    forbid_extra_attrs: bool,
+
+    /// Compute with steady state attractors only
+    #[clap(short, long, takes_value = false)]
+    steady_states: bool,
+
+    /// Goal model to check for in the resulting ensemble
+    #[clap(short, long)]
+    goal_model: Option<String>,
+}
+
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 4 {
-        println!("3 arguments expected, got {}", args.len() - 1);
-        println!("Usage: ./inference-attractors model_file attractor_data forbid_extra_attrs");
-        return;
-    }
-    if !(args[3].as_str() == "false" || args[3].as_str() == "true") {
-        println!(
-            "Invalid argument \"{}\", it must be either \"true\" or \"false\"",
-            args[3]
-        );
-        println!("Usage: ./inference-attractors model_file attractor_data (true | false)");
-        return;
-    }
-    let forbid_extra_attrs = match args[3].as_str() {
-        "false" => false,
-        _ => true, // we need match to be exhaustive
+    let args = Arguments::parse();
+    let goal_aeon_string: Option<String> = match args.goal_model {
+        None => None,
+        Some(file_name) => Some(read_to_string(file_name.to_string()).unwrap()),
     };
 
-    // TODO: make this automatic from CLI
-    //let goal_aeon_string = Some(read_to_string("inference_goal_model.aeon".to_string()).unwrap());
-    let goal_aeon_string: Option<String> = None;
-
-    let data_file = File::open(Path::new(args[2].as_str())).unwrap();
+    let data_file = File::open(Path::new(args.attractor_data_path.as_str())).unwrap();
     let reader = BufReader::new(&data_file);
     let data: Vec<String> = reader.lines().collect::<Result<_, _>>().unwrap();
-    let aeon_string = read_to_string(args[1].clone()).unwrap();
+    let aeon_string = read_to_string(args.model_path).unwrap();
 
     let start = SystemTime::now();
     perform_inference_with_attractors_specific(
         data,
         aeon_string,
-        false,
-        forbid_extra_attrs,
+        args.steady_states,
+        args.forbid_extra_attrs,
         goal_aeon_string,
     );
     println!("Elapsed time: {}ms", start.elapsed().unwrap().as_millis());
