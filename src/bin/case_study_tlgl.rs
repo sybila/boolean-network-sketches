7b4f20b07646a58e3bf921628e8f9c7d5396526f
Ondřej Huvar <492849@mail.muni.cz>
2022-12-17 11:39:04 +0100

    update TLGL case study

--- src/bin/case_study_tlgl.rs
+++ src/bin/case_study_tlgl.rs
@@ -13,6 +13,7 @@ use network_sketches::utils::{
 
 use std::fs::read_to_string;
 use std::time::SystemTime;
+use biodivine_lib_param_bn::biodivine_std::traits::Set;
 
 /// Structure to collect CLI arguments
 #[derive(Parser)]
@@ -21,18 +22,126 @@ use std::time::SystemTime;
     about="Inference case study regarding T-LGL model."
 )]
 struct Arguments {
-    /// Use the variant of the model with completely unspecified logic
+    /// Use the refined variant of the sketch
     #[clap(short, long, takes_value = false)]
-    fully_unspecified_logic: bool,
+    refined_sketch: bool,
 
     /// Print summarizing info regarding candidate set
     #[clap(short, long, takes_value = false)]
     summarize_candidates: bool,
 }
 
+fn case_study_part_1() {
+    let start = SystemTime::now();
+    let aeon_string =
+        read_to_string("benchmark_models/TLGL_reduced/TLGL_reduced_no_updates.aeon").unwrap();
+
+    // create the partially specified BN
+    let bn = BooleanNetwork::try_from(aeon_string.as_str()).unwrap();
+    println!("Loaded model with {} vars.", bn.num_vars());
+    let mut graph = SymbolicAsyncGraph::new(bn, 2).unwrap();
+    println!("Model has {} parameters.", graph.symbolic_context().num_parameter_vars());
+    println!("----------");
+
+    // apply static constraints
+    println!(
+        "After applying static constraints, {} concretizations remain.",
+        graph.mk_unit_colors().approx_cardinality(),
+    );
+    println!("Elapsed time: {}ms", start.elapsed().unwrap().as_millis());
+    println!("----------");
+
+    // define data observation and corresponding dynamic property
+    let diseased_attractor = "~Apoptosis_ & S1P & sFas & ~Fas & ~Ceramide_ & ~Caspase & MCL1 & ~BID_ & ~DISC_ & FLIP_ & ~IFNG_ & GPCR_";
+    let formulae: Vec<String> = vec![mk_attractor_formula_nonspecific(diseased_attractor.to_string()), ];
+
+    // apply dynamic constraints
+    graph = apply_constraints_and_restrict(formulae, graph, "attractor ensured");
+    println!(
+        "After ensuring attractor presence, {} concretizations remain.",
+        graph.mk_unit_colors().approx_cardinality(), // graph has restricted unit colors to satisfying ones
+    );
+    println!("Elapsed time: {}ms", start.elapsed().unwrap().as_millis());
+    println!("----------");
+
+    let attrs_all_candidates = model_check_formula("!{x}: AG EF {x}".to_string(), &graph).unwrap();
+    println!("Attractors for all candidates computed");
+    println!("Elapsed time: {}ms", start.elapsed().unwrap().as_millis());
+    println!("----------");
+
+    let programmed_cell_death_formula = "Apoptosis_ & ~S1P & ~sFas & ~Fas & ~Ceramide_ & ~Caspase & ~MCL1 & ~BID_ & ~DISC_ & ~FLIP_ & ~CTLA4_ & ~TCR & ~IFNG_ & ~CREB & ~P2 & ~SMAD_ & ~GPCR_ & ~IAP_";
+    let pcd = model_check_formula(programmed_cell_death_formula.to_string(), &graph).unwrap();
+    let colors_not_pcd = graph.mk_unit_colors().minus(&attrs_all_candidates.intersect(&pcd).colors());
+    println!("There are {} colors without programmed cell death attractor, such as:", colors_not_pcd.approx_cardinality());
+    println!("{}", graph.pick_witness(&colors_not_pcd).to_bnet(false).unwrap());
+    println!("----------");
+
+    let unwanted_state_formula = "Apoptosis_ & (S1P | sFas | Fas | Ceramide_ | Caspase  | MCL1 | BID_ | DISC_  | FLIP_ | CTLA4_ | TCR | IFNG_ | CREB  | P2 | SMAD_ | GPCR_ | IAP_)";
+    let unwanted_states = model_check_formula(unwanted_state_formula.to_string(), &graph).unwrap();
+    let colors_with_unwanted_states = attrs_all_candidates.intersect(&unwanted_states).colors();
+    println!("There are {} colors with unwanted states in attractors, such as:", colors_with_unwanted_states.approx_cardinality());
+    println!("{}", graph.pick_witness(&colors_with_unwanted_states).to_bnet(false).unwrap());
+    println!("----------");
+
+    println!("Elapsed time: {}ms", start.elapsed().unwrap().as_millis());
+}
+
+fn case_study_part_2(summarize_candidates: bool) {
+    let aeon_string =
+        read_to_string("benchmark_models/TLGL_reduced/TLGL_reduced_partial_updates.aeon").unwrap();
+
+    // create the partially specified BN
+    let bn = BooleanNetwork::try_from(aeon_string.as_str()).unwrap();
+    println!("Loaded model with {} vars.", bn.num_vars());
+    let mut graph = SymbolicAsyncGraph::new(bn, 2).unwrap();
+    println!("Model has {} parameters.", graph.symbolic_context().num_parameter_vars());
+    println!("----------");
+
+    // apply static constraints
+    println!(
+        "After applying static constraints, {} concretizations remain.",
+        graph.mk_unit_colors().approx_cardinality(),
+    );
+    println!("----------");
+
+    // define both observations and corresponding properties
+
+    let diseased_attractor = "~Apoptosis_ & S1P & sFas & ~Fas & ~Ceramide_ & ~Caspase & MCL1 & ~BID_ & ~DISC_ & FLIP_ & ~IFNG_ & GPCR_";
+    let healthy_attractor = "Apoptosis_ & ~S1P & ~sFas & ~Fas & ~Ceramide_ & ~Caspase & ~MCL1 & ~BID_ & ~DISC_ & ~FLIP_ & ~CTLA4_ & ~TCR & ~IFNG_ & ~CREB & ~P2 & ~SMAD_ & ~GPCR_ & ~IAP_";
+
+    let formulae: Vec<String> = vec![
+        mk_steady_state_formula_specific(healthy_attractor.to_string()),
+        mk_attractor_formula_nonspecific(diseased_attractor.to_string()),
+    ];
+
+    // first ensure attractor existence
+    graph = apply_constraints_and_restrict(formulae, graph, "attractor ensured");
+    println!(
+        "After ensuring attractor presence, {} concretizations remain.",
+        graph.mk_unit_colors().approx_cardinality(),
+    );
+
+    // then prohibit all other attractors
+    let attr_set = vec![healthy_attractor.to_string(), diseased_attractor.to_string()];
+    let formula = mk_forbid_other_attractors_formula(attr_set);
+    let inferred_colors = model_check_formula(formula, &graph).unwrap().colors();
+    println!(
+        "{} suitable networks found in total",
+        inferred_colors.approx_cardinality()
+    );
+    println!("----------");
+
+    println!("{}", graph.pick_witness(&inferred_colors).to_bnet(false).unwrap());
+    if summarize_candidates {
+        summarize_candidates_naively(&graph, inferred_colors.clone());
+    }
+}
+
+
 /// Analysis of the T Cell Survival Network (T-LGL model)
 /// Infers BNs from sketch including 2 attractor observations
-fn case_study(fully_unspecified_logic: bool, summarize_candidates: bool) {
+#[allow(dead_code)]
+fn case_study_original(fully_unspecified_logic: bool, summarize_candidates: bool) {
     let aeon_string = if fully_unspecified_logic {
         read_to_string("benchmark_models/TLGL_reduced/TLGL_reduced_no_updates.aeon").unwrap()
     } else {
@@ -99,16 +208,20 @@ fn case_study(fully_unspecified_logic: bool, summarize_candidates: bool) {
 fn main() {
     let args = Arguments::parse();
 
-    let fully_unspecified_logic = if args.fully_unspecified_logic {
-        "fully unspecified update logic"
+    let refined_sketch = if args.refined_sketch {
+        "refined variant of the sketch"
     } else {
-        "partially specified update logic"
+        "initial variant of the sketch"
     };
-    println!("MODE: {}", fully_unspecified_logic);
+    println!("MODE: {}", refined_sketch);
 
-    let start = SystemTime::now();
-    case_study(args.fully_unspecified_logic, args.summarize_candidates);
-    println!("Elapsed time: {}ms", start.elapsed().unwrap().as_millis());
+    if args.refined_sketch {
+        case_study_part_2(true);
+    } else {
+        case_study_part_1()
+    }
+
+    //case_study_original(args.fully_unspecified_logic, args.summarize_candidates);
 }
 
 #[cfg(test)]
